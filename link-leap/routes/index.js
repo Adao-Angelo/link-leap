6909ce4465a87b4b8a36f446f8fc71deecb938e9
Adao-Angelo <adaobegginer@gmail.com>
2024-03-26 21:41:20 +0100

    create a index page

--- link-leap/routes/index.js
+++ link-leap/routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+function generateCode(url) {
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqerstuvwxyz123456789";
+  for (let e = 0; e < 5; e++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Link-leap" });
 });
 
 module.exports = router;
+
+router.post("/new", (req, res, next) => {
+  const url = req.body.url;
+  const code = generateCode();
+
+  res.send("http://localhost:3000/" + code);
+});
